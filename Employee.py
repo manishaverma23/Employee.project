627f2b2ba4246417a5afa5b8cb6b6835719c8530
Rajmouli123 <vermamanisha2611@gmail.com>
2025-01-03 19:33:11 +0530

    first commit

---
+++ Employee.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+#Database Connection 
+
+DB_CONFIG = {
+    "dbname":"employee_db",
+    "user":"postgres",
+    "password":"password",
+    "host":"localhost",
+    "port":5432
+}
+
+def connect_db():
+    """Connect to the PostgreSQL database"""
+    try:
+        conn = psycopg2.connect(**DB_CONFIG)
+        print("Connection Successful")
+        return conn
+    except Exception as e:
+        print("Error connecting to database",e)
+        return None
+
+connect_db()
