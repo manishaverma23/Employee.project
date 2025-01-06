ebd1afe32b9f871928cddf882fecfc2e52a93887
Rajmouli123 <vermamanisha2611@gmail.com>
2025-01-06 19:48:01 +0530

    first commit

--- Employee.py
+++ Employee.py
@@ -21,3 +21,22 @@ def connect_db():
         return None
 
 # connect_db()
+
+def add_employee(name,position,salary):
+    """ Add new employee to database"""
+    query= "INSERT INTO emp(name,position,salary) VALUES(%s,%s,%s)"
+    conn=connect_db()
+    if conn:
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(query,(name,position,salary))
+                conn.commit()
+                print("Employee added succesfully")
+        except Exception as e:
+            print(" Error adding employee",e)
+        
+        finally:
+            conn.close()
+
+add_employee('rama','manager',50000)
+
